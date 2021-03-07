6240162d1777aa043c90f1e61cf95f1c50ca640a
Sarfaraz Alam <sarfarazalam.sa53@gmail.com>
2021-03-08 04:02:55 +0530

    removed search bug, chatlist bug, final version of app for evaulation

---
+++ securechatv2_final_for_evaluation/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:securechatv2/services/auth.dart';
+import 'package:securechatv2/views/home.dart';
+import 'package:securechatv2/views/signin.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FutureBuilder(
+        future: AuthMethods().getCurrentUser(),
+        builder: (context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasData) {
+            return Home();
+          } else {
+            return SignIn();
+          }
+        },
+      ),
+    );
+  }
+}
